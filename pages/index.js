52498f630a307a2e004eceb400c6a0fea44712cb
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-04 18:47:41 +0600

    added the logo

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ export default function Home() {
   return (
     <main className="h-screen min-h-[max-content]">
       <div className="relative text-4xl h-[80%] md:h-[80%]  lg:h-[92.5%] min-h-[max-content] bg-white">
-        <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat bg-width-[10%]">
-          <p className="p">
-
-          </p>
-        </div>
+        <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat" />
         <div className="absolute top-0 left-0 right-0 h-[100%] z-[2] 	text-white">
+          <img src="/logo.png" alt="logo" className="h-[20px] md:h-[30px] lg:h-[40px]" />
         </div>
       </div>
       <div className="text-4xl text-center h-screen">
