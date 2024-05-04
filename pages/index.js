f5bef5e28cdef2c7ee81b94091d86c9397ac5052
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-04 18:33:19 +0600

    added responsive hero background image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-
-
 export default function Home() {
   return (
-    <main
-      className={``}
-    >
-     
+    <main className="h-screen min-h-[max-content]">
+      <div className="relative text-4xl h-[80%] md:h-[80%]  lg:h-[92.5%] min-h-[max-content] bg-white">
+        <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat bg-width-[10%]">
+          <p className="p">
+
+          </p>
+        </div>
+        <div className="absolute top-0 left-0 right-0 h-[100%] z-[2] 	text-white">
+        </div>
+      </div>
+      <div className="text-4xl text-center h-screen">
+      </div>
     </main>
   );
 }
