0092d37858524d6f3f902280f6d43117e4e06c0c
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-04 20:16:50 +0600

    added a responsive navbar
    a slider box for mobile application
    abd a link for pc application

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { CrossIcon, MenuIcon, XIcon } from 'lucide-react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Header = () => (
   <Head>
@@ -11,8 +13,11 @@ const Header = () => (
 );
 
 export default function Home() {
+
+  const [openNavbar, setOpenNavbar] = useState(false);
+
   return (
-    <main className="h-screen min-h-[max-content]">
+    <main className={`h-screen min-h-[max-content] ${openNavbar ? 'overflow-y-hidden' : 'overflow-y-auto'} `}>
       <Header />
       <div className="relative text-4xl h-[80%] md:h-[80%]  lg:h-[92.5%] min-h-[max-content] bg-white">
         <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat" />
@@ -21,14 +26,49 @@ export default function Home() {
             <img src="/logo.png" alt="logo" className="display-block mr-auto cursor-pointer h-[20px] md:h-[30px] lg:h-[40px]" />
 
             <Link href="/contact">
-              <button className="rounded-none bg-[#BFB1C4] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#A498A7] hover:opacity-[0.95] transition duration-200 ease-in-out">
+              <button className="hidden md:block rounded-none bg-[#BFB1C4] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#A498A7] hover:opacity-[0.95] transition duration-200 ease-in-out">
                 Apply To Lift Up
               </button>
             </Link>
+
+            {
+              !openNavbar &&
+              <button onClick={() => setOpenNavbar(!openNavbar)} className="block md:hidden rounded-none">
+                <MenuIcon color='#ffffff' size={32} strokeWidth={1.5} />
+              </button>
+
+            }
+
           </div>
         </div>
+
+
+
+
+        <div
+          className={
+            'fixed md:hidden bg-white transition-all duration-600 h-screen w-[100vw] fixed z-[3] ' +
+            (openNavbar ? 'left-0' : 'left-[100vw]')
+          }
+        >
+          <div className="p-8 lg:p-16 flex items-center justify-end">
+
+            {
+              openNavbar &&
+              <button onClick={() => setOpenNavbar(!openNavbar)} className="block md:hidden rounded-none">
+                <XIcon color='#000000' size={32} strokeWidth={1.5} />
+              </button>
+            }
+
+          </div>
+
+        </div>
+
+
+
       </div>
-      <div className="text-4xl text-center h-screen">
+      <div className="text-4xl text-center h-screen bg-red-100">
+
       </div>
     </main>
   );
