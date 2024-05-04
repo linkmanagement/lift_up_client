c6348bd6d093e26502d3f54a8421e0bb064f404e
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-04 18:57:58 +0600

    updated the favicon

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
       <div className="relative text-4xl h-[80%] md:h-[80%]  lg:h-[92.5%] min-h-[max-content] bg-white">
         <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat" />
         <div className="absolute top-0 left-0 right-0 h-[100%] z-[2] 	text-white">
-          <img src="/logo.png" alt="logo" className="h-[20px] md:h-[30px] lg:h-[40px]" />
+          <div className="p-16">
+            <img src="/logo.png" alt="logo" className="cursor-pointer h-[20px] md:h-[30px] lg:h-[40px]" />
+          </div>
         </div>
       </div>
       <div className="text-4xl text-center h-screen">
