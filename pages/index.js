569b0507e190ef21adf67556a12fc7ccaadb30bd
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-08 00:27:41 +0600

    u

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,23 @@ import { useState } from 'react';
 
 const services = [
   {
-    serviceName: "OnlyFans Content & Marketing Management",
-    content: "Want to take your page to the ultimate arousal? We work to create a consistent content calendar for you to follow. So you can play hard, whilst we work hard. We will manage copywriting, scheduling, editing and distributing so all you need to do is send us your content, and we do the rest.",
-    image: "/service_1.jpeg"
+    serviceName: "Exclusive content creation,marketing & management",
+    content: "Looking to elevate your onlyfans page to the next level of excitement? Our team crafts a seamless content schedule, allowing you to indulge in your passions while we handle the details. From crafting engaging copy to scheduling, editing, and distribution, we take care of it all. Just share your content, and leave the rest to us.",
+    image: "/service_1.jpg"
   },
   {
-    serviceName: "Let Us Do The Talking",
-    content: "We look after relationships with your fans by managing all chats on a 24/7 basis. That means you can focus on creating exciting content whilst we keep your customers busy and happy.",
+    serviceName: "Leave the talking to us.",
+    content: "We handle fan interactions 24/7, ensuring you can focus on content creation while we keep your audience engaged and satisfied.",
     image: "/service_2.jpg"
   },
   {
-    serviceName: "Grow Your Fanbase",
-    content: "Looking to get new fans? Our team work day and night to bring new customers to your page. We manage all your social media accounts and dating apps, growing your influence and getting paying fans to subscribe.",
+    serviceName: "Expand your fanbase.",
+    content: "Seeking to attract new fans? Our dedicated team works tirelessly to drive traffic to your page. We manage your social media accounts and dating apps, expanding your reach and converting followers into paying subscribers.",
     image: "/service_3.jpg"
   },
   {
-    serviceName: "Seductively Unique",
-    content: "Every creator has their own spicy something, and that’s what makes your personal brand so unique. We will work with you to develop your brand and help you to focus on what’s really needed to bring your profile to life.",
+    serviceName: "Uniquely captivating",
+    content: "Your personal brand is as unique as it is enticing. We collaborate with you to refine your brand and pinpoint the essentials that breathe life into your profile.",
     image: "/service_4.jpg"
   }
 ]
@@ -74,8 +74,8 @@ function Nav({ openNavbar, setOpenNavbar }) {
       <img src="/logo.png" alt="logo" className="display-block mr-auto cursor-pointer h-[20px] md:h-[30px] lg:h-[40px]" />
 
       <Link href="/apply">
-        <button className="hidden md:block rounded-none bg-[#BFB1C4] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#A498A7] hover:opacity-[0.95] transition duration-200 ease-in-out">
-          Apply To Lift Up
+        <button className="hidden md:block rounded-none bg-[#BFD8AF] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#E0FFCC] hover:opacity-[0.95] transition duration-200 ease-in-out">
+          Apply Now
         </button>
       </Link>
 
@@ -95,18 +95,18 @@ function HeroContent() {
   return (
     <div className="p-6 md:p-8 lg:p-16 flex flex-col w-[90%] md:max-w-[40vw] gap-4 min-h-[max-content] mb-[40px] md:mb-[100px] lg:mb-[150px]">
       <p className="text-[54px] font-bold leading-[1.1]">
-        {`Reaching your full OnlyFans potential starts `}
-        <span className='text-[#BFB1C4]'>
-          {`here.`}
+        {`Unlocking your complete potential on OnlyFans begins `}
+        <span className='text-[#BFD8AF]'>
+          {`now.`}
         </span>
       </p>
       <p className="text-[16px] font-semibold leading-[1.5]">
-        {`Join the leading OnlyFans Management Agency.`}
+        {`Become part of the premier OnlyFans Management Agency.`}
       </p>
 
       <Link href="/apply">
-        <button className="rounded-none bg-[#BFB1C4] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#A498A7] hover:opacity-[0.95] transition duration-200 ease-in-out">
-          Apply To Lift Up
+        <button className="rounded-none bg-[#BFD8AF] text-black text-[13px] px-6 py-2 rounded-lg font-medium hover:bg-[#E0FFCC] hover:opacity-[0.95] transition duration-200 ease-in-out">
+          Apply Now
         </button>
       </Link>
     </div>
@@ -117,7 +117,7 @@ function HeroSection({ openNavbar, setOpenNavbar }) {
 
   return (
     <div className="relative text-4xl h-[90%] md:h-[90%] lg:h-[92.5%] bg-white">
-      <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/hero_cropped_mobile.jpg')] md:bg-[url('/hero_cropped_tab.jpg')] lg:bg-[url('/hero_cropped.jpg')] bg-cover bg-center	bg-no-repeat" />
+      <div className="saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9] absolute top-0 left-0 right-0 h-[100%] z-[1] bg-[url('/services_alt_1_phone.jpg')] md:bg-[url('/services_alt_1_tab.jpg')] lg:bg-[url('/services_alt_1_hero.jpg')] bg-cover bg-center	bg-no-repeat" />
       <div className="absolute flex flex-col justify-between top-0 left-0 right-0 h-[100%] z-[2] text-white">
         <Nav openNavbar={openNavbar} setOpenNavbar={setOpenNavbar} />
         <HeroContent />
@@ -132,7 +132,7 @@ function SecondarySection() {
     <div className="min-h-[60vh] text-center flex items-center justify-center bg-white">
       <div className='w-[100%] md:w-[85%] lg:w-[55%] m-[40px] md:m-[64px] lg:m-[128px]'>
         <p className="text-[54px] font-bold leading-[1.1] text-black">
-          We will help you to join the 0.1% top-tier content creators.
+          Let us guide you to the top 0.1% of content creators.
         </p>
       </div>
     </div>
@@ -161,7 +161,7 @@ function LeftImageServices({
         </p>
         <Link href="/apply">
           <button className="w-[max-content] rounded-none bg-[#000000] text-white text-[13px] px-6 py-4 rounded-lg font-medium hover:bg-[#1b1b1b] hover:opacity-[0.95] transition duration-200 ease-in-out">
-            Apply To Lift Up
+            Apply Now
           </button>
         </Link>
       </div>
@@ -187,7 +187,7 @@ function RightImageServices({
         </p>
         <Link href="/apply">
           <button className="w-[max-content] rounded-none bg-[#000000] text-white text-[13px] px-6 py-4 rounded-lg font-medium hover:bg-[#1b1b1b] hover:opacity-[0.95] transition duration-200 ease-in-out">
-            Apply To Lift Up
+            Apply Now
           </button>
         </Link>
       </div>
@@ -202,7 +202,7 @@ function RightImageServices({
 
 function ServicesSection() {
   return (
-    <div className='bg-[#BFB1C4] flex flex-col items-center justify-center min-h-[60vh]  p-8 md:p-16 gap-32'>
+    <div className='bg-[#BFD8AF] flex flex-col items-center justify-center min-h-[60vh]  p-8 md:p-16 gap-32'>
       <p className="text-center text-[54px] font-bold leading-[1.1] text-black">
         Our Services
       </p>
@@ -220,10 +220,10 @@ function QuoteSection() {
     <div className="min-h-[60vh] text-center flex items-center justify-center bg-white">
       <div className='w-[100%] md:w-[85%] lg:w-[55%] m-[40px] md:m-[64px] lg:m-[128px]'>
         <p className="text-[30px] md:text-[38px] font-regular leading-[1.1] text-black">
-          “Extremely professional team, and easy to work with! They are very hands-on so I can be hands-off. They really brought my profile to a whole new level.”
+            "Outstandingly professional and effortless collaboration. Their hands-on approach allowed me to take a step back while they worked wonders, bringing my profile to new heights."
         </p>
         <p className='mt-[20px] text-[16px] font-regular leading-[1.5] text-black'>
-          — NORA L.
+          — KELLY S.
         </p>
       </div>
     </div>
@@ -235,12 +235,12 @@ function ApplySection() {
     <div className="relative min-h-[60vh] text-center flex items-center justify-center gap-4">
       <div className="bg-[url('/apply.jpg')] bg-cover bg-center bg-no-repeat w-[100%] h-[100%] absolute top-0 left-0 right-0 z-[1] saturate-[0.9] blur-[0.7px] opacity-[0.9] brightness-90 contrast-[0.9]" />
       <div className='w-[100%] md:w-[85%] lg:w-[55%] m-[40px] md:m-[64px] lg:m-[128px] z-[2]'>
-        <p className="text-[44px] md:text-[54px] font-bold leading-[1.1] text-black">
-          Join The Team
+        <p className="text-[44px] md:text-[54px] font-bold leading-[1.1] text-white">
+        Become a part of our team.
         </p>
         <Link href="/apply">
           <button className="mt-[100px] w-full md:w-[max-content] rounded-none bg-[#000000] text-white text-[13px] px-6 py-4 rounded-lg font-medium hover:bg-[#1b1b1b] hover:opacity-[0.95] transition duration-200 ease-in-out">
-            Apply To Lift Up
+            Apply Now
           </button>
         </Link>
       </div>
